ed352bbb10d9189105ad94cecf3fec274568555e
AirCasta <icejudgement@163.com>
2017-05-17 18:50:33 +0800

    name and icon

--- app/src/main/java/com/example/weathertest/WeatherActivity.java
+++ app/src/main/java/com/example/weathertest/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.weathertest;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -24,6 +25,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.weathertest.gson.Forecast;
 import com.example.weathertest.gson.Weather;
+import com.example.weathertest.service.AutoUpdateService;
 import com.example.weathertest.utils.HttpUtil;
 import com.example.weathertest.utils.Utility;
 
@@ -63,8 +65,10 @@ public class WeatherActivity extends BaseActivity {
         if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
+            Log.e("change","true");
         }
         setContentView(R.layout.activity_weather);
         drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -121,7 +125,7 @@ public class WeatherActivity extends BaseActivity {
     public void requestWeather(String weatherId) {
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" +
                 weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
-        Log.e("request", "1");
+        //Log.e("request", "1");
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -131,15 +135,15 @@ public class WeatherActivity extends BaseActivity {
                     public void run() {
                         Toast.makeText(WeatherActivity.this, "Get weather " +
                                 "failed", Toast.LENGTH_SHORT).show();
+                        swipeRefresh.setRefreshing(false);
                     }
                 });
-                swipeRefresh.setRefreshing(false);
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
-                Log.e("weather", responseText);
+                //Log.e("weather", responseText);
                 final Weather weather = Utility.handleWeatherResponse(responseText);
                 runOnUiThread(new Runnable() {
                     @Override
@@ -154,9 +158,9 @@ public class WeatherActivity extends BaseActivity {
                             Toast.makeText(WeatherActivity.this, "Get weather " +
                                     "failed", Toast.LENGTH_SHORT).show();
                         }
+                        swipeRefresh.setRefreshing(false);
                     }
                 });
-                swipeRefresh.setRefreshing(false);
             }
         });
         loadBingPic();
@@ -195,6 +199,8 @@ public class WeatherActivity extends BaseActivity {
         carwashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent=new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     private void loadBingPic() {
@@ -223,4 +229,5 @@ public class WeatherActivity extends BaseActivity {
             }
         });
     }
+
 }
